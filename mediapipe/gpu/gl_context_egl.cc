e9016703a2c15157cd721789bb3e5e6c60bd0f47
Mykola Yakovliev <mya@2021.ai>
2022-09-29 16:25:42 +0300

    opencv fix

--- mediapipe/gpu/gl_context_egl.cc
+++ mediapipe/gpu/gl_context_egl.cc
@@ -125,7 +125,7 @@ absl::Status GlContext::CreateContextInternal(EGLContext share_context,
 #ifdef MEDIAPIPE_OMIT_EGL_WINDOW_BIT
       EGL_PBUFFER_BIT,
 #else
-      EGL_PBUFFER_BIT | EGL_WINDOW_BIT,
+      EGL_PBUFFER_BIT,
 #endif
       EGL_RED_SIZE, 8,
       EGL_GREEN_SIZE, 8,
--- setup.py
+++ setup.py
@@ -217,13 +217,13 @@ class BuildBinaryGraphs(build_ext.build_ext):
   def run(self):
     _check_bazel()
     binary_graphs = [
-        'face_detection/face_detection_short_range_cpu',
-        'face_detection/face_detection_full_range_cpu',
-        'face_landmark/face_landmark_front_cpu',
-        'hand_landmark/hand_landmark_tracking_cpu',
-        'holistic_landmark/holistic_landmark_cpu', 'objectron/objectron_cpu',
-        'pose_landmark/pose_landmark_cpu',
-        'selfie_segmentation/selfie_segmentation_cpu'
+        'face_detection/face_detection_short_range_gpu',
+        'face_detection/face_detection_full_range_gpu',
+        'face_landmark/face_landmark_front_gpu',
+        'hand_landmark/hand_landmark_tracking_gpu',
+        'holistic_landmark/holistic_landmark_gpu', 'objectron/objectron_gpu',
+        'pose_landmark/pose_landmark_gpu',
+        'selfie_segmentation/selfie_segmentation_gpu'
     ]
     for binary_graph in binary_graphs:
       sys.stderr.write('generating binarypb: %s\n' %
@@ -238,7 +238,9 @@ class BuildBinaryGraphs(build_ext.build_ext):
         'build',
         '--compilation_mode=opt',
         '--copt=-DNDEBUG',
-        '--define=MEDIAPIPE_DISABLE_GPU=1',
+        # '--define=MEDIAPIPE_DISABLE_GPU=1',
+        '--copt=-DMESA_EGL_NO_X11_HEADERS',
+        '--copt=-DEGL_NO_X11',
         '--action_env=PYTHON_BIN_PATH=' + _normalize_path(sys.executable),
         os.path.join('mediapipe/modules/', graph_path),
     ]
@@ -295,7 +297,9 @@ class BuildExtension(build_ext.build_ext):
         'build',
         '--compilation_mode=opt',
         '--copt=-DNDEBUG',
-        '--define=MEDIAPIPE_DISABLE_GPU=1',
+        # '--define=MEDIAPIPE_DISABLE_GPU=1',
+        '--copt=-DMESA_EGL_NO_X11_HEADERS',
+        '--copt=-DEGL_NO_X11',
         '--action_env=PYTHON_BIN_PATH=' + _normalize_path(sys.executable),
         str(ext.bazel_target + '.so'),
     ]
