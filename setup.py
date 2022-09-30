17ef09eecbbd8883b0f081ff04e4d8e9f404b075
Mykola Yakovliev <mya@2021.ai>
2022-09-30 09:41:55 +0300

    added more config

--- setup.py
+++ setup.py
@@ -221,9 +221,10 @@ class BuildBinaryGraphs(build_ext.build_ext):
         'face_detection/face_detection_full_range_gpu',
         'face_landmark/face_landmark_front_gpu',
         'hand_landmark/hand_landmark_tracking_gpu',
-        'holistic_landmark/holistic_landmark_gpu', 'objectron/objectron_gpu',
+        # 'holistic_landmark/holistic_landmark_gpu',
+        'objectron/objectron_gpu',
         'pose_landmark/pose_landmark_gpu',
-        'selfie_segmentation/selfie_segmentation_gpu'
+        # 'selfie_segmentation/selfie_segmentation_gpu'
     ]
     for binary_graph in binary_graphs:
       sys.stderr.write('generating binarypb: %s\n' %
